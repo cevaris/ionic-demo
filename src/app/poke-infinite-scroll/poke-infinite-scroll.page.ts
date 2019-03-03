4cad1267ceabe740ec02f6cd10e234f5c238dbee
Adam Cardenas <cevaris@gmail.com>
2019-03-03 08:05:30 -0800

    got ionic4 infinite-scroll working

--- src/app/poke-infinite-scroll/poke-infinite-scroll.page.ts
+++ src/app/poke-infinite-scroll/poke-infinite-scroll.page.ts
@@ -1,10 +1,10 @@
 import {Component, ViewChild} from '@angular/core';
 import {IonInfiniteScroll} from "@ionic/angular";
-import {range} from "lodash";
+import {min, range} from "lodash";
 
 const minPoke = 1;
 const maxPoke = 809;
-const pageSize = 20;
+const pageSize = 30;
 const rootURL = "https://assets.pokemon.com/assets/cms2/img/pokedex/full/[ID].png";
 const pokeUrl = (id) => {
     return rootURL.replace("[ID]", id.padStart(3, 0));
@@ -16,51 +16,29 @@ const pokeUrl = (id) => {
     styleUrls: ['poke-infinite-scroll.page.scss'],
 })
 export class PokeInfiniteScrollPage {
-    // @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-    items = [];
+    @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
+    data = [];
 
     constructor() {
-        for (let i = 0; i < 30; i++) {
-            this.items.push( this.items.length );
-        }
+        this.data = this.data.concat(range(minPoke, this.data.length + pageSize, 1));
     }
 
-    doInfinite(event) {
-        console.log('Begin async operation');
-
+    loadData(event) {
         setTimeout(() => {
-            for (let i = 0; i < 30; i++) {
-                this.items.push( this.items.length );
-            }
-
-            console.log('Async operation has ended');
+            this.data = this.data.concat(range(this.data.length - 1, min([this.data.length + pageSize, maxPoke]), 1));
             event.target.complete();
+            console.log('loadData', 'complete');
+
+            // App logic to determine if all data is loaded
+            // and disable the infinite scroll
+            if (this.data.length == maxPoke) {
+                event.target.disabled = true;
+                console.log('loadData', 'disabled');
+            }
         }, 500);
     }
-}
 
-// export class PokeInfiniteScrollPage {
-//     @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
-//
-//     data = new Array(809);
-//     currPage = 0;
-//
-//     constructor() {
-//         this.data = range(minPoke, maxPoke, 1);
-//     }
-//
-//     loadData(event) {
-//         console.log("loadData", this.currPage, event);
-//         setTimeout(() => {
-//             console.log('Done loading', this.currPage, event);
-//             event.target.complete();
-//             this.currPage++;
-//
-//             // App logic to determine if all data is loaded
-//             // and disable the infinite scroll
-//             if (this.currPage > (maxPoke / this.currPage)) {
-//                 event.target.disabled = true;
-//             }
-//         }, 500);
-//     }
-// }
+    toggleInfiniteScroll() {
+        this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
+    }
+}
